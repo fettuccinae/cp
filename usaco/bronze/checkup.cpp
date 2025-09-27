69933d61b3f17075728b08dc8244b8b9c4cab0d0
Junaid <139873457+fettuccinae@users.noreply.github.com>
2025-09-27 23:44:21 +0530

    yummy

--- usaco/bronze/checkup.cpp
+++ usaco/bronze/checkup.cpp
@@ -5,8 +5,7 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 #include <cstdio>
 
-/*
-*/
+
 int32_t main() {
     //freopen("input.txt","r",stdin);
     //freopen("output.out","w",stdout);
