0483f5d015785e36555b5225610f76a8335d2874
junaid <shaikjunaid323@gmail.com>
2025-10-28 16:47:15 +0000

    word

---
+++ usaco/bronze/les_vignes.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+
+/*
+
+
+
+
+*/
+int32_t main() {
+    fastio;
+    int tt; cin >> tt;
+    while (tt--) {
+        int n; cin >> n;
+        string s; cin >> s;
+        // cout << s << endl;
+        int res = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (s[i] != s[i + 1]) {
+                res++;
+            }
+        }
+        if (s[0] == '1') {
+            res++;
+        }
+        cout << res << endl;
+
+
+    }
+    return 0;
+}
--- usaco/bronze/then_mooed.cpp
+++ usaco/bronze/then_mooed.cpp
@@ -6,14 +6,43 @@ using namespace std;
 #include <cstdio>
 
 /*
+https://usaco.org/index.php?page=viewproblem2&cpid=1445
 */
 int32_t main() {
     //freopen("input.txt","r",stdin);
     //freopen("output.out","w",stdout);
     fastio;
-    int tt; cin >> tt;
-    while (tt--) {
-        
+    int n, f; cin >> n >> f;
+    string yap; cin >> yap;
+
+    map<string, int>rodeo;
+    set<string>moos;
+
+    for (int i = 0; i < n - 2; i++) {
+        if (yap[i + 1] == yap[i + 2]) {
+            string temp = yap.substr(i, 3);
+            rodeo[temp]++;
+        }
+    }
+    for (auto it : rodeo) {
+        cout << it.first << " " << it.second << endl;
+        if (it.first[0] != it.first[1] && it.second >= f) {
+            it.second = -1;
+            moos.insert(it.first);
+        }
+    }
+    for (auto it = rodeo.begin(); it != rodeo.end(); ++it) {
+        if (it->second == -1 || (it->first[0] == it->first[1])) continue;
+        for (auto jt = rodeo.begin(); jt != rodeo.end(); ++jt) {
+            if (jt->second == -1 || (it->first == jt->first)) continue;
+            if (it->second + jt->second >= f && it->first[1] == jt->first[1]) {
+                moos.insert(it->first);
+            }
+        }
+    }
+    cout << moos.size() << endl;
+    for (auto it : moos) {
+        cout << it << endl;
     }
     return 0;
 }
