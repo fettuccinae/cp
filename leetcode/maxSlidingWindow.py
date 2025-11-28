3fdb5c5bf9a49a88ff07fd0d367e3e605f21e261
junaid <shaikjunaid323@gmail.com>
2025-11-28 15:33:52 +0530

    van

---
+++ leetcode/maxSlidingWindow.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        i,res = 0,[]
+        j = i+k
+        while j <= len(nums):
+            res.append(max(nums[i:j]))
+            j+=1
+            i+=1
+        return (res)
