b3422efccac873f7de89fbd3a4e09c60650ab114
junaid <shaikjunaid323@gmail.com>
2025-11-28 15:25:37 +0530

    panic

--- usaco/bronze/outofplace.cpp
+++ usaco/bronze/outofplace.cpp
@@ -11,30 +11,20 @@ int32_t main(){
     freopen("outofplace.in","r",stdin);
     freopen("outofplace.out","w",stdout);
     fastio;
-    int N; cin >> N;
-    set<int>un;
-    vector<int>data;
-    int oop_index;
-    for (int i = 0; i < N; i++) {
-        int t; cin >> t;
-        if (un.count(t) != 0) {
-            data.push_back(t);
-            un.insert(t);
+    int n; cin >> n;
+    vector<int>main, comp;
+    for (int i = 0; i < n; i++) {
+        int temp; cin >> temp;
+        main.push_back(temp);
+        comp.push_back(temp);
+    }
+    sort(comp.begin(), comp.end());
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        if (main[i] != comp[i]) {
+            res++;
         }
     }
-    int n = data.size();
-    for (int i = 0; i < n-1; i++){
-        if (data[i] > data[i + 1]) {
-            
-        }
-    }
-    int new_index;
-    for (int i = 0; i < data.size(); i++) {
-        if (data[i] < data[oop_index]){
-            new_index = i;
-        }
-    }
-    cout << new_index - oop_index
-
+    cout << res - 1 << endl;
     return 0;
 }
