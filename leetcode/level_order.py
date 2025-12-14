42b7d7323eb0bcf7c775c71dfcfc4825aad5cc0f
Junaid <139873457+fettuccinae@users.noreply.github.com>
2025-12-14 12:42:35 +0000

    Bye bye

---
+++ leetcode/level_order.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if not root:
+            return res
+        queue = deque([root])
+        while queue:
+            trus = []
+            for i in range(len(queue)):
+                node = queue.popleft()
+                trus.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(trus)
+        return(res)
+     
