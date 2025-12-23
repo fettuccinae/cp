7b1270c450786f7f194fc19ba82f8c2ea35581f9
junaid <shaikjunaid124@gmail.com>
2025-12-23 15:30:10 +0000

    i

---
+++ leetcode/n_node_from_end.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        old = None
+        new = head
+        for i in range(n):
+            new = new.next
+        while new:
+            if old:
+                old = old.next
+            else:
+                old = head
+            new = new.next
+        try:
+            old.next = old.next.next
+        except:
+            head = head.next
+
+        return head
+
+
+
+
+
+
+
