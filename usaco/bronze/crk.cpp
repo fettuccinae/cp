95edcb44d749af0941b42eb821c041701e90a527
junaid <shaikjunaid323@gmail.com>
2026-01-22 06:50:01 +0530

    jr

---
+++ usaco/bronze/crk.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#include <cstdio>
+
+/*
+Wolf                      M1M2M3
+Goat       (1, 2)         C1C2C3
+Cabb
+Human
+Not allowed -> WG, GC
+
+M1 M2     4
+            m3 c3 
+C1 C2
+*/
+int32_t main() {
+    //freopen("input.in","r",stdin);
+    //freopen("output.out","w",stdout);
+    fastio;
+    int m_count_left = 0;
+    int m_count_right = 3;
+    int c_count_left = 0;
+    int c_count_right = 3;
+    vector<char>elems_left = { 'w', 'g', 'c', 'h' };
+    vector<char>elems_right = {'0', '1', '2', '7', '8', '9'};
+    bool left = true;
+
+
+
+
+    return 0;
+}
---
+++ usaco/bronze/crk_easy1.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+#include <cstdio>
+
+/*
+wolf
+goat     <1, 2>      desta
+leaf
+man
+*/
+int32_t main(){
+    //freopen("input.in","r",stdin);
+    //freopen("output.out","w",stdout);
+    fastio;
+    vector<char>left={'w', 'g', 'l', 'm'}, right;
+    for (int i = 0; i , )
+    return 0;
+}
